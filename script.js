d7eeb2e2788b6ad9626f5a4ec0dba8c6610c4cc3
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-17 11:41:58 +0100

    Rename variables and add containersize-change-function

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const container = document.querySelector("#container");
+const inputContainerSize = document.querySelector("#inputContainerSize");
 
-let x = 16;
+let edgeLength = 16;
+let containerSize = 500;
 
-function drawCanvas(size) {
-    let total = x * x;
-    let pixelHeight = (container.scrollHeight / size) + "px";
-    let pixelWidth = (container.scrollWidth / size) + "px";
+inputContainerSize.addEventListener("keydown", (event) => {
+    const keyname = event.key;
 
-    for (let i = 0; i < total; i++) {
-        const pixel = document.createElement("div");
-        pixel.style.height = pixelHeight;
-        pixel.style.width = pixelWidth;
+    if (keyname === "Enter") {
+        containerSize = inputContainerSize.value;
+        console.log("Containersize: " + containerSize);
+        drawContainer();
+    }
+})
+
+function drawPixel(edgeLengthInCubes) {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    let sizeInCubes = edgeLengthInCubes * edgeLengthInCubes;
+    let cubeHeight = (container.scrollHeight / edgeLengthInCubes) + "px";
+    let cubeWidth = (container.scrollWidth / edgeLengthInCubes) + "px";
+
+    for (let i = 0; i < sizeInCubes; i++) {
+        const cube = document.createElement("div");
+        cube.style.height = cubeHeight;
+        cube.style.width = cubeWidth;
         console.log("Container height: " + container.scrollHeight);
-        console.log("Pixel height: " + pixel.style.height);
+        console.log("Cube height: " + cube.style.height);
         console.log("Container width: " + container.scrollWidth);
-        console.log("Pixel width: " + pixel.style.width);
-        container.appendChild(pixel);
+        console.log("Cube width: " + cube.style.width);
+        container.appendChild(cube);
     }
 }
 
-drawCanvas(x);
+function drawContainer(sizeInPixel = containerSize) {
+    console.log("Container drawn");
+    container.style.height = sizeInPixel + "px";
+    container.style.width = sizeInPixel + "px";
+    drawPixel(edgeLength);
+} 
+
+
+drawContainer();
